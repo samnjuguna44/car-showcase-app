6c0450c1f1bce9222061209a1b86ccd7597b3543
Samson Njuguna <96062644+samnjuguna44@users.noreply.github.com>
2023-07-14 16:24:11 +0300

    created SearchBar component and functionality

---
+++ components/SearchBar.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
+import SearchManufacturer from "./SearchManufacturer";
+
+const SearchBar = () => {
+  const [manufacturer, setManuFacturer] = useState("");
+
+  const handleSearch = () => {};
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManuFacturer}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
