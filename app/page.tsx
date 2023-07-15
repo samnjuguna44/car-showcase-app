93e0aacea6cb7189e07af1c100a5cae9792b5132
Samson Njuguna <96062644+samnjuguna44@users.noreply.github.com>
2023-07-15 16:05:11 +0300

    created CarCard component and added Rapi API  client to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,44 @@
-import { CustomFilter, Hero, SearchBar } from '@/components'
-import Image from 'next/image'
+import { fetchCars } from "../utils";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
-      <div className='mt-12 padding-x padding-y max-width' id='discover'>
-        <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore out cars you might like</p>
         </div>
 
-        <div className='home__filters'>
+        <div className="home__filters">
           <SearchBar />
 
-          <div className='home__filter-container'>
-            <CustomFilter title='fuel'  />
-            <CustomFilter title='year' />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
-      </div>  
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
     </main>
-  )
+  );
 }
